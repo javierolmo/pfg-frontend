0e7b4fb32d628c98484cb09bbbfbfba451ada693
Javi <olmo.injerto.javier@gmail.com>
2021-06-01 15:36:26 +0200

    Default env

---
+++ src/assets/env/env.js
@@ -0,0 +1,7 @@
+(function(window) {
+  window.env = window.env || {};
+
+  // Environment variables
+  window["env"]["apiUrl"] = "http://47.61.163.104:64001/api";
+  window["env"]["docUrl"] = "http://47.61.163.104:64001/swagger-ui/";
+})(this);
